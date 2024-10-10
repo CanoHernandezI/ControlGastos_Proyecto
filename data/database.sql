b42d35ae3fd0783825555743c9eeeca7dfa3df63
“RamirezLuisManuel” <luismanuelr245@gmail.com>
2024-10-09 23:28:53 -0600

    actualizacion de api nueva en mi rama

--- data/database.sql
+++ data/database.sql
@@ -17,8 +17,6 @@ CREATE TABLE Usuario (
     Usuario CHAR(20) NOT NULL UNIQUE,
     Contrasena VARCHAR(30) NOT NULL,
     CONSTRAINT chk_tipo_usuario CHECK (TipoUsuario IN (1, 2))
-    Contrasena VARCHAR(30) NOT NULL,
-    CONSTRAINT chk_tipo_usuario CHECK (TipoUsuario IN (1, 2))
 );
 
 CREATE TABLE Presupuesto (
